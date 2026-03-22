88b189c5e083166808e8db5af88bec69c984e30c
Efe Ozarslan <s32666@pjwstk.edu.pl>
2026-03-22 16:33:02 +0100

    new rental class was added

--- Models/Camera.cs
+++ Models/Camera.cs
@@ -3,13 +3,12 @@
 public class Camera : Equipment
 {
     public int LensWidth { get; set; }
-    
     public int BatteryCapacity { get; set; }
     
-    public Camera(string name, int LensWidth, int BatteryCapacity)
+    public Camera(string name, int lensWidth, int batteryCapacity)
         : base(name)
     {
-        LensWidth = LensWidth;
-        BatteryCapacity = BatteryCapacity;
+        LensWidth = lensWidth;
+        BatteryCapacity = batteryCapacity;
     }
 }
---
+++ Models/Rental.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace APBD_TASK2.Models;
+
+public class Rental
+{
+    private static int _nextId = 1;
+    
+    public int Id { get; }
+    public User RentedBy { get; set; }
+    public Equipment RentedEquipment { get; set; }
+    public DateTime RentalDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? ReturnDate { get; set; } 
+
+    public Rental(User user, Equipment equipment, int durationInDays)
+    {
+        Id = _nextId++;
+        RentedBy = user;
+        RentedEquipment = equipment;
+        RentalDate = DateTime.Now;
+        DueDate = RentalDate.AddDays(durationInDays);
+    }
+}
