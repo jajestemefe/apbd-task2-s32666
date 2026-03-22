2dbc5c9d02264c03310434e76d57b70d7da182ec
Efe Ozarslan <s32666@pjwstk.edu.pl>
2026-03-22 16:35:08 +0100

    Improvements on the interface IRentalServices

--- Interfaces/IRentalServices.cs
+++ Interfaces/IRentalServices.cs
@@ -1,6 +1,15 @@
-﻿namespace APBD_TASK2.Interfaces;
+﻿using APBD_TASK2.Models;
+using System.Collections.Generic;
+
+namespace APBD_TASK2.Interfaces;
 
 public interface IRentalServices
 {
+    void AddEquipment(Equipment equipment);
+    void AddUser(User user);
+    List<Equipment> GetAllEquipments();
     
+    bool RentEquipment(User user, Equipment equipment, int days);
+    
+    double ReturnEquipment(Equipment equipment, int daysLate = 0);
 }
